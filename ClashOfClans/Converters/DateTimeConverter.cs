b3833255e82a724d1b478e94d8a3cba0ad9a5d41
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-23 20:54:18 +0200

    Adding a DateTime converter for received timestamps

---
+++ ClashOfClans/Converters/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ClashOfClans.Converters
+{
+    internal class DateTimeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var dateTimeString = reader.Value as string;
+
+            //                                         '20190319T022619.000Z'
+            return DateTime.ParseExact(dateTimeString, "yyyyMMddTHHmmss.fffZ", System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
