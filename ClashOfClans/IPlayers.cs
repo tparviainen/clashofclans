83f7f039035819e4c05693ffc949490902a46777
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-23 21:22:37 +0200

    Support for player API

---
+++ ClashOfClans/IPlayers.cs
@@ -0,0 +1,15 @@
+﻿using ClashOfClans.Models;
+using System.Threading.Tasks;
+
+namespace ClashOfClans
+{
+    public interface IPlayers
+    {
+        /// <summary>
+        /// Get player information
+        /// </summary>
+        /// <param name="playerTag">Tag of the player to retrieve</param>
+        /// <returns></returns>
+        Task<PlayerDetail> GetAsync(string playerTag);
+    }
+}
