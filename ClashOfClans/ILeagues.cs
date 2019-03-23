d86ea6ab0192ab88c3db02f04714c0e154dc3dd1
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-23 21:20:44 +0200

    Support for league API

---
+++ ClashOfClans/ILeagues.cs
@@ -0,0 +1,40 @@
+﻿using ClashOfClans.Core;
+using ClashOfClans.Models;
+using System.Threading.Tasks;
+
+namespace ClashOfClans
+{
+    public interface ILeagues
+    {
+        /// <summary>
+        /// List leagues
+        /// </summary>
+        /// <param name="query">Query parameters</param>
+        /// <returns></returns>
+        Task<LeagueList> GetAsync(Query query = null);
+
+        /// <summary>
+        /// Get league information
+        /// </summary>
+        /// <param name="leagueId">Identifier of the league to retrieve</param>
+        /// <returns></returns>
+        Task<League> GetAsync(int? leagueId);
+
+        /// <summary>
+        /// Get league seasons
+        /// </summary>
+        /// <param name="leagueId">Identifier of the league whose seasons retrieve</param>
+        /// <param name="query">Query parameters</param>
+        /// <returns></returns>
+        Task<LeagueSeasonList> GetSeasonsAsync(int? leagueId, Query query = null);
+
+        /// <summary>
+        /// Get league season rankings
+        /// </summary>
+        /// <param name="leagueId">Identifier of the league whose season rankings retrieve</param>
+        /// <param name="seasonId">Identifier of the season whose rankings retrieve</param>
+        /// <param name="query">Query parameters</param>
+        /// <returns></returns>
+        Task<SeasonPlayerRankingList> GetSeasonsAsync(int? leagueId, string seasonId, Query query = null);
+    }
+}
