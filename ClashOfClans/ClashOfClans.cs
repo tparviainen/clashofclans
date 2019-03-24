1d3fe290a44a49b905ae4251a27c02a3a1487efb
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-24 13:13:20 +0200

    Clash of Clans API

---
+++ ClashOfClans/ClashOfClans.cs
@@ -0,0 +1,20 @@
+﻿using ClashOfClans.Core;
+
+namespace ClashOfClans
+{
+    public class ClashOfClans
+    {
+        public ClashOfClans(string token)
+        {
+            Clans = new Clans(token);
+            Locations = new Locations(token);
+            Leagues = new Leagues(token);
+            Players = new Players(token);
+        }
+
+        public IClans Clans { get; }
+        public ILocations Locations { get; }
+        public ILeagues Leagues { get; }
+        public IPlayers Players { get; }
+    }
+}
