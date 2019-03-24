fa849600f59729e0959ad8ca6b1ffed5b4b9b819
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-24 12:59:56 +0200

    Exception class to handle API specific errors

---
+++ ClashOfClans/Core/ClashOfClansException.cs
@@ -0,0 +1,15 @@
+﻿using ClashOfClans.Models;
+using System;
+
+namespace ClashOfClans.Core
+{
+    public class ClashOfClansException : Exception
+    {
+        public ClashOfClansException(Error error)
+        {
+            Error = error;
+        }
+
+        public Error Error { get; }
+    }
+}
