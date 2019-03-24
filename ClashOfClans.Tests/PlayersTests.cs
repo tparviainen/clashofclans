3fdaeb8ed20f020b66c79cdeec5a5ecf9e57cd0d
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-24 14:52:05 +0200

    Unit tests for Players

---
+++ ClashOfClans.Tests/PlayersTests.cs
@@ -0,0 +1,23 @@
+﻿using ClashOfClans.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace ClashOfClans.Tests
+{
+    [TestClass]
+    public class PlayersTests : TestsBase
+    {
+        [TestMethod]
+        public async Task GetPlayerInformation()
+        {
+            // Arrange
+            var players = new Players(Token);
+
+            // Act
+            var player = await players.GetAsync(PlayerTag);
+
+            // Assert
+            Assert.IsNotNull(player);
+        }
+    }
+}
