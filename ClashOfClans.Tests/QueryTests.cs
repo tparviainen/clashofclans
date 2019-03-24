81df5f38899edc1de9abb298ee0d5bb267077871
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-24 14:52:25 +0200

    Unit tests for Query class

---
+++ ClashOfClans.Tests/QueryTests.cs
@@ -0,0 +1,37 @@
+﻿using ClashOfClans.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClashOfClans.Tests
+{
+    [TestClass]
+    public class QueryTests
+    {
+        [TestMethod]
+        public void QueryLimitSetTo10()
+        {
+            // Arrange
+            var query = new Query();
+
+            // Act
+            query.Limit = 10;
+
+            // Assert
+            Assert.IsTrue(query.ToString().Contains("limit=10"));
+        }
+
+        [TestMethod]
+        public void QueryLimitAndAfterSet()
+        {
+            // Arrange
+            var query = new Query();
+
+            // Act
+            query.Limit = 20;
+            query.After = "12345";
+
+            // Assert
+            Assert.IsTrue(query.ToString().Contains("limit=20"));
+            Assert.IsTrue(query.ToString().Contains("after=12345"));
+        }
+    }
+}
