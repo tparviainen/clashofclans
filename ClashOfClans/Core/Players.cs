2466ea8f7ae6712f5420ff5e1a19b19304b9b76e
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-24 13:11:15 +0200

    Implementation of the Players API

---
+++ ClashOfClans/Core/Players.cs
@@ -0,0 +1,26 @@
+﻿using ClashOfClans.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace ClashOfClans.Core
+{
+    public class Players : ClashOfClansBase, IPlayers
+    {
+        public Players(string token) : base(token)
+        {
+        }
+
+        // GET /players/{playerTag}
+        public async Task<PlayerDetail> GetAsync(string playerTag)
+        {
+            if (string.IsNullOrWhiteSpace(playerTag))
+            {
+                throw new ArgumentNullException(nameof(playerTag));
+            }
+
+            var uri = $"players/{playerTag}";
+
+            return await RequestAsync<PlayerDetail>(uri);
+        }
+    }
+}
