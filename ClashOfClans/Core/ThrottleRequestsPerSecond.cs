3999d54b7b3c3f80d10315cd76f864f158b96967
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-27 18:26:09 +0200

    Throttling based on requests per second

---
+++ ClashOfClans/Core/ThrottleRequestsPerSecond.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClashOfClans.Core
+{
+    public class ThrottleRequestsPerSecond : IThrottleRequests
+    {
+        private DateTime _nextAllowedApiCallTime;
+        private int _delayBetweenApiCalls;
+
+        public ThrottleRequestsPerSecond(int maxRequestsPerSecond)
+        {
+            if (maxRequestsPerSecond <= 0)
+            {
+                throw new ArgumentException(nameof(maxRequestsPerSecond));
+            }
+
+            _nextAllowedApiCallTime = DateTime.Now;
+            _delayBetweenApiCalls = 1000 / maxRequestsPerSecond;
+        }
+
+        public async Task WaitAsync()
+        {
+            var now = DateTime.Now;
+
+            if (now < _nextAllowedApiCallTime)
+            {
+                var millisecondsDelay = (int)(_nextAllowedApiCallTime - now).TotalMilliseconds;
+                await Task.Delay(millisecondsDelay);
+            }
+
+            _nextAllowedApiCallTime = DateTime.Now.AddMilliseconds(_delayBetweenApiCalls);
+        }
+    }
+}
