44aeb30d6ed2d37bb61a1ed09712872231d8d0a1
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-27 18:24:12 +0200

    Interface definition for throttling functionality

---
+++ ClashOfClans/Core/IThrottleRequests.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ClashOfClans.Core
+{
+    public interface IThrottleRequests
+    {
+        /// <summary>
+        /// Blocks the execution of the current thread if throttling limit reached
+        /// </summary>
+        Task WaitAsync();
+    }
+}
