ab97bff691a66a26ef71f5f634d267557a9ec88f
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-03-31 17:42:31 +0300

    Add tests for QueryClan class

--- ClashOfClans.Tests/QueryTests.cs
+++ ClashOfClans.Tests/QueryTests.cs
@@ -10,28 +10,72 @@ namespace ClashOfClans.Tests
         public void QueryLimitSetTo10()
         {
             // Arrange
-            var query = new Query();
+            var query = new Query
+            {
+                Limit = 10
+            };
 
             // Act
-            query.Limit = 10;
+            var queryString = query.ToString();
 
             // Assert
-            Assert.IsTrue(query.ToString().Contains("limit=10"));
+            Assert.AreEqual("?limit=10", queryString);
         }
 
         [TestMethod]
         public void QueryLimitAndAfterSet()
         {
             // Arrange
-            var query = new Query();
+            var query = new Query
+            {
+                Limit = 20,
+                After = "12345"
+            };
 
             // Act
-            query.Limit = 20;
-            query.After = "12345";
+            var queryString = query.ToString();
 
             // Assert
-            Assert.IsTrue(query.ToString().Contains("limit=20"));
-            Assert.IsTrue(query.ToString().Contains("after=12345"));
+            Assert.AreEqual('?', queryString[0]);
+            Assert.IsTrue(queryString.Contains("&"));
+            Assert.IsTrue(queryString.Contains("limit=20"));
+            Assert.IsTrue(queryString.Contains("after=12345"));
+        }
+
+        [TestMethod]
+        public void QuaryClansForSpecificLocation()
+        {
+            // Arrange
+            var query = new QueryClans
+            {
+                LocationId = 42
+            };
+
+            // Act
+            var queryString = query.ToString();
+
+            // Assert
+            Assert.AreEqual("?locationid=42", queryString);
+        }
+
+        [TestMethod]
+        public void QueryClansForSpecificLocationAndLimit()
+        {
+            // Arrange
+            var query = new QueryClans
+            {
+                LocationId = 42,
+                Limit = 10
+            };
+
+            // Act
+            var queryString = query.ToString();
+
+            // Assert
+            Assert.AreEqual('?', queryString[0]);
+            Assert.IsTrue(queryString.Contains("&"));
+            Assert.IsTrue(queryString.Contains("locationid=42"));
+            Assert.IsTrue(queryString.Contains("limit=10"));
         }
     }
 }
