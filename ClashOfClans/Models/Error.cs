02ea94326da7a20005038040ebcba4cb07f64a23
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-04-02 18:02:08 +0300

    Override ToString for better debugging capability

--- ClashOfClans/Models/Error.cs
+++ ClashOfClans/Models/Error.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ClashOfClans.Models
+﻿namespace ClashOfClans.Models
 {
     public class Error
     {
         public string Reason;
 
         public string Message;
+
+        public override string ToString()
+        {
+            return $"{Message}: {Reason}";
+        }
     }
 }
