d3240a001f9ac147c6a0aaae126a46b7fb8ac2a9
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-04-19 17:04:55 +0300

    Update documentation of the location indexer

--- src/ClashOfClans/Models/Utilities/LocationList.cs
+++ src/ClashOfClans/Models/Utilities/LocationList.cs
@@ -4,6 +4,18 @@ namespace ClashOfClans.Models
 {
     public partial class LocationList
     {
+        /// <summary>
+        /// Provides an access to <see cref="Location"/> data for the named location.
+        /// </summary>
+        /// <param name="name">Location name</param>
+        /// <example>
+        /// <code>
+        /// var coc = new ClashOfClansApi(token);
+        /// var locations = await coc.Locations.GetAsync();
+        /// var location = locations["Finland"];
+        /// </code>
+        /// </example>
+        /// <returns>Location data</returns>
         public Location this[string name]
         {
             get => Items?.SingleOrDefault(l => l.Name == name);
