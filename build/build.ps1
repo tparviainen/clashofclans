16cf6f7602e1c827ebda391479803f672bac07ea
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-04-19 13:42:44 +0300

    Move build files to build folder

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,5 @@
 $packageVersion = "3.0.0"
-$project = ".\ClashOfClans\ClashOfClans.csproj"
+$project = "..\src\ClashOfClans\ClashOfClans.csproj"
 
 dotnet build --configuration Release $project
 dotnet pack -p:PackageVersion=$packageVersion --configuration Release $project
