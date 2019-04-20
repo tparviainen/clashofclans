7d6eb8eb8c9c9d2ddca2cd15fe0ef3f7d023706d
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-04-20 16:27:22 +0300

    Add more examples about how to use API

--- src/ClashOfClans.App/Program.cs
+++ src/ClashOfClans.App/Program.cs
@@ -3,6 +3,7 @@ using ClashOfClans.Core;
 using ClashOfClans.Models;
 using ClashOfClans.Search;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClashOfClans.App
@@ -15,27 +16,42 @@ namespace ClashOfClans.App
             var playerTag = "[player tag]";
             var token = "[your own unique API key]";
 
-            var query = new QueryClans
-            {
-                Name = "Phoenix",
-                Limit = 10,
-                WarFrequency = WarFrequency.Never
-            };
-
             try
             {
                 var coc = new ClashOfClansApi(token);
 
-                var clan = await coc.Clans.GetAsync(clanTag);           // Get Clan Information
-                var warLog = await coc.Clans.GetWarLogAsync(clanTag);   // Retrieve Clans Clan War Log
-                var player = await coc.Players.GetAsync(playerTag);     // Get Player Information
-                var leagueList = await coc.Leagues.GetAsync();          // List Leagues
-                var locationList = await coc.Locations.GetAsync();      // List Locations
-                var searchResult = await coc.Clans.GetAsync(query);     // Search Clans
+                // Get Clan Information
+                var clan = await coc.Clans.GetAsync(clanTag);
+                Console.WriteLine($"Clan '{clan.Name}' is a level {clan.ClanLevel} clan and has {clan.Members} members");
 
-                // Usage ...
+                // Retrieve Clans Clan War Log
+                var warLog = await coc.Clans.GetWarLogAsync(clanTag);
+                var latest = warLog.Items.First();
+                Console.WriteLine($"Clan '{latest.Clan.Name}' had a war against '{latest.Opponent.Name}' that ended {latest.EndTime} UTC");
+
+                // Get Player Information
+                var player = await coc.Players.GetAsync(playerTag);
                 Console.WriteLine($"Player '{player.Name}' is in clan '{player.Clan?.Name}'");
-                Console.WriteLine($"Clan '{clan.Name}' has {clan.Members} members");
+
+                // Get League Information
+                var leagues = await coc.Leagues.GetAsync();
+                var league = leagues["Legend League"];
+                Console.WriteLine($"'{league.Name}' has an ID {league.Id}");
+
+                // Get Location Information
+                var locations = await coc.Locations.GetAsync();
+                var location = locations["Finland"];
+                Console.WriteLine($"'{location.Name}' has an ID {location.Id} and country code '{location.CountryCode}'");
+
+                // Search Clans
+                var query = new QueryClans
+                {
+                    Limit = 1,
+                    WarFrequency = WarFrequency.Always
+                };
+                var clans = await coc.Clans.GetAsync(query);
+                var warClan = clans.Items.First();
+                Console.WriteLine($"Clan '{warClan.Name}' has {warClan.WarWins} wins, {warClan.WarLosses} losses and {warClan.WarTies} draws");
             }
             catch (ClashOfClansException ex)
             {
