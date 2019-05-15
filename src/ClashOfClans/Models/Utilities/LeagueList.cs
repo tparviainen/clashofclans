c0eed7fab32fd3e07c7a4cddadfaed90fd4306c4
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-05-15 20:22:39 +0300

    Add missing example tags

--- src/ClashOfClans/Models/Utilities/LeagueList.cs
+++ src/ClashOfClans/Models/Utilities/LeagueList.cs
@@ -8,11 +8,13 @@ namespace ClashOfClans.Models
         /// Provides an access to <see cref="League"/> data for the named league.
         /// </summary>
         /// <param name="name">League name</param>
+        /// <example>
         /// <code>
         /// var coc = new ClashOfClansApi(token);
         /// var leagues = await coc.Leagues.GetAsync();
         /// var league = leagues["Legend League"];
         /// </code>
+        /// </example>
         /// <returns>League data</returns>
         public League this[string name]
         {
