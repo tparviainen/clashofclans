e7697a4a596d89cb9f49127000baa40dabfc3040
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-05-15 19:53:25 +0300

    Add documentation for League indexer

--- src/ClashOfClans/Models/Utilities/LeagueList.cs
+++ src/ClashOfClans/Models/Utilities/LeagueList.cs
@@ -4,6 +4,16 @@ namespace ClashOfClans.Models
 {
     public partial class LeagueList
     {
+        /// <summary>
+        /// Provides an access to <see cref="League"/> data for the named league.
+        /// </summary>
+        /// <param name="name">League name</param>
+        /// <code>
+        /// var coc = new ClashOfClansApi(token);
+        /// var leagues = await coc.Leagues.GetAsync();
+        /// var league = leagues["Legend League"];
+        /// </code>
+        /// <returns>League data</returns>
         public League this[string name]
         {
             get => Items?.SingleOrDefault(l => l.Name == name);
