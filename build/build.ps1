756e6418dbf56a97ac12800ce117f1423d940fd7
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-06-29 11:14:30 +0300

    Update NuGet to version 3.0.4

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,6 @@
-$packageVersion = "3.0.3"
+$packageVersion = "3.0.4"
 $project = "..\src\ClashOfClans\ClashOfClans.csproj"
 $configuration = "Release"
 
 dotnet clean -c $configuration $project
-dotnet build -c $configuration $project
-dotnet pack -p:PackageVersion=$packageVersion -c $configuration $project
+dotnet pack -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
