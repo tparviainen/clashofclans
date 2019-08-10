20fe043fd2296816bde78a1c45c019f4af9085e4
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-08-10 08:40:44 +0300

    Add "internal" class for configuration options
    Internal configuration class provides more configurable entries that
    the implementation uses internally and that are not meant for user
    to configure.

---
+++ src/ClashOfClans/Core/ClashOfClansOptionsInternal.cs
@@ -0,0 +1,18 @@
+﻿using ClashOfClans.Validation;
+
+namespace ClashOfClans.Core
+{
+    internal class ClashOfClansOptionsInternal : ClashOfClansOptions
+    {
+        internal IThrottleRequests _throttleRequests;
+        internal readonly Validator _validator;
+
+        public ClashOfClansOptionsInternal(string token) : base(token)
+        {
+            _validator = new Validator();
+            _throttleRequests = new ThrottleRequestsPerSecond(MaxRequestsPerSecond);
+
+            _validator.ValidateToken(token);
+        }
+    }
+}
