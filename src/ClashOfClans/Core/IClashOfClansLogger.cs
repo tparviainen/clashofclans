47554d7eb75e50061c7fa3dd2ec994a49fc17ff3
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-08-10 08:06:16 +0300

    Define the logging interface
    This is the logging interface that can be implemented by the
    library users to get diagnostics messages.

---
+++ src/ClashOfClans/Core/IClashOfClansLogger.cs
@@ -0,0 +1,14 @@
+﻿namespace ClashOfClans.Core
+{
+    /// <summary>
+    /// Represents a type used to perform logging
+    /// </summary>
+    public interface IClashOfClansLogger
+    {
+        /// <summary>
+        /// Writes a log entry
+        /// </summary>
+        /// <param name="message">Log message</param>
+        void Log(string message);
+    }
+}
