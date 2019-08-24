a67e5696ac55f605820bebd07a49a1f46ec3cb92
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-08-24 11:38:25 +0300

    Add unit tests to cover missing models

---
+++ src/ClashOfClans.Tests/ModelTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Linq;
+
+namespace ClashOfClans.Tests
+{
+    [TestClass]
+    public class ModelTests : ModelTestsBase
+    {
+        [TestMethod]
+        public void ModelsMissingFromAssembly()
+        {
+            // Arrange
+            var count = 0;
+
+            // Act
+            foreach (var modelName in _apiModels)
+            {
+                if (!_assemblyModels.Any(m => m.Name == modelName))
+                {
+                    Trace.WriteLine($"Missing: {modelName}");
+                    count++;
+                }
+            }
+
+            // Assert
+            Assert.IsTrue(count == 0);
+        }
+
+        [TestMethod]
+        public void ExtraModelsInAssembly()
+        {
+            // Arrange
+            var count = 0;
+
+            // Act
+            foreach (var assemblyModel in _assemblyModels)
+            {
+                var modelName = assemblyModel.Name;
+                if (!_apiModels.Contains(modelName))
+                {
+                    Trace.WriteLine($"Extra: {modelName}");
+                    count++;
+                }
+            }
+
+            // Assert
+            Assert.IsTrue(count == 0);
+        }
+    }
+}
---
+++ src/ClashOfClans.Tests/ModelTestsBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ClashOfClans.Tests
+{
+    public class ModelTestsBase
+    {
+        protected IEnumerable<string> _apiModels;
+        protected IEnumerable<Type> _assemblyModels;
+
+        public ModelTestsBase()
+        {
+            _apiModels = GetModelsFromFiles().OrderBy(n => n);
+            _assemblyModels = GetModelsFromAssembly("ClashOfClans.Models").OrderBy(t => t.Name);
+        }
+
+        private ICollection<string> GetModelsFromFiles()
+        {
+            var files = Directory.GetFiles("Models");
+            var models = new List<string>();
+
+            foreach (var file in files)
+            {
+                models.AddRange(GetModelsFromText(file));
+            }
+
+            return models.Distinct().ToList();
+        }
+
+        private IEnumerable<string> GetModelsFromText(string path)
+        {
+            var content = File.ReadAllText(path);
+            var lists = Regex.Matches(content, @"\b([A-Z]\w+)\b");
+            var models = new List<string>();
+
+            foreach (Match listItem in lists)
+            {
+                models.Add(listItem.Value);
+            }
+
+            return models.Distinct();
+        }
+
+        private ICollection<Type> GetModelsFromAssembly(string ns)
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(t => t.GetTypes())
+                .Where(t => t.IsClass && t.IsPublic && t.Namespace == ns);
+
+            return types.ToList();
+        }
+    }
+}
