4309bf5a07576c01698448197fa2eb74452438cd
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-08-28 18:31:16 +0300

    Remove unnecessary base class

--- src/ClashOfClans/Models/Clan.cs
+++ src/ClashOfClans/Models/Clan.cs
@@ -1,9 +1,42 @@
 ﻿namespace ClashOfClans.Models
 {
-    public class Clan : ClanBase
+    public class Clan : Identity
     {
         public string Description { get; set; }
 
         public ClanMember[] MemberList { get; set; }
+
+        public Type? Type { get; set; }
+
+        public Location Location { get; set; }
+
+        public UrlContainer BadgeUrls { get; set; }
+
+        public int? ClanLevel { get; set; }
+
+        public int? ClanPoints { get; set; }
+
+        public int? ClanVersusPoints { get; set; }
+
+        /// <summary>
+        /// Minimum trophies to join
+        /// </summary>
+        public int? RequiredTrophies { get; set; }
+
+        public WarFrequency? WarFrequency { get; set; }
+
+        public int? WarWinStreak { get; set; }
+
+        public int? WarWins { get; set; }
+
+        public bool? IsWarLogPublic { get; set; }
+
+        public int? Members { get; set; }
+
+        public int? WarTies { get; set; }
+
+        public int? WarLosses { get; set; }
+
+        public override string ToString() => $"{nameof(Clan)}: {base.ToString()}";
     }
 }
