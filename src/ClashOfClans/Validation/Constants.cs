f3b1aefd1d168b55286f1fe8b88b2dd789011628
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-10-02 18:40:02 +0300

    Add constants to internal class

---
+++ src/ClashOfClans/Validation/Constants.cs
@@ -0,0 +1,15 @@
+﻿namespace ClashOfClans.Validation
+{
+    internal static class Constants
+    {
+        /// <summary>
+        /// CWL group that is in 'Preparation' state contains invalid war tags
+        /// </summary>
+        public const string InvalidWarTag = "#0";
+
+        /// <summary>
+        /// All tags (war tag, clan tag, player tag) starts with '#' character
+        /// </summary>
+        public const string TagStartChar = "#";
+    }
+}
