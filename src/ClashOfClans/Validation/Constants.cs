c2932eac6815fef732af97b954119b0fb34e9c63
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-10-02 18:54:55 +0300

    Add min query name length const

--- src/ClashOfClans/Validation/Constants.cs
+++ src/ClashOfClans/Validation/Constants.cs
@@ -11,5 +11,11 @@
         /// All tags (war tag, clan tag, player tag) starts with '#' character
         /// </summary>
         public const string TagStartChar = "#";
+
+        /// <summary>
+        /// If name is used as part of search query, it needs to be at least
+        /// three characters long.
+        /// </summary>
+        public const int MinQueryNameLength = 3;
     }
 }
