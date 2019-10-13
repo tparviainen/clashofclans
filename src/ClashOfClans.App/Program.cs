17682d5d219b8f35ebe4ed8e5abc3748321622cf
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-10-13 11:34:40 +0300

    Update main to use individual examples

--- src/ClashOfClans.App/Program.cs
+++ src/ClashOfClans.App/Program.cs
@@ -1,10 +1,6 @@
-﻿using ClashOfClans;
-using ClashOfClans.Core;
-using ClashOfClans.Models;
-using ClashOfClans.Search;
+﻿using ClashOfClans.Core;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClashOfClans.App
@@ -17,41 +13,25 @@ namespace ClashOfClans.App
 
             try
             {
-                var coc = new ClashOfClansApi(token);
+                // Access clan specific information
+                var clansExamples = new ClansExamples(token, clanTag);
+                await clansExamples.GetClanInformation();
+                await clansExamples.RetrieveClansClanWarLog();
 
-                // Get Clan Information
-                var clan = await coc.Clans.GetAsync(clanTag);
-                Console.WriteLine($"Clan '{clan.Name}' is a level {clan.ClanLevel} clan and has {clan.Members} members");
+                // Access player specific information
+                var playersExamples = new PlayersExamples(token, playerTag);
+                await playersExamples.GetPlayerInformation();
 
-                // Retrieve Clans Clan War Log
-                var warLog = await coc.Clans.GetWarLogAsync(clanTag);
-                var latest = warLog.Items.First();
-                Console.WriteLine($"Clan '{latest.Clan.Name}' had a war against '{latest.Opponent.Name}' that ended {latest.EndTime} UTC");
+                // Access league information
+                var leaguesExamples = new LeaguesExamples(token);
+                await leaguesExamples.ListLeagues();
+                await leaguesExamples.GetLeagueSeasons();
+                await leaguesExamples.GetLeagueSeasonRankings();
 
-                // Get Player Information
-                var player = await coc.Players.GetAsync(playerTag);
-                Console.WriteLine($"Player '{player.Name}' is in clan '{player.Clan?.Name}'");
-
-                // Get League Information
-                var leagues = await coc.Leagues.GetAsync();
-                var league = leagues["Legend League"];
-                Console.WriteLine($"'{league.Name}' has an ID {league.Id}");
-
-                // Get Location Information
-                var locations = await coc.Locations.GetAsync();
-                var location = locations["Finland"];
-                Console.WriteLine($"'{location.Name}' has an ID {location.Id} and country code '{location.CountryCode}'");
-
-                // Search Clans
-                var query = new QueryClans
-                {
-                    Limit = 1,
-                    WarFrequency = WarFrequency.Always
-                };
-
-                var clans = await coc.Clans.GetAsync(query);
-                var warClan = clans.Items.First();
-                Console.WriteLine($"Clan '{warClan.Name}' has {warClan.WarWins} wins, {warClan.WarLosses} losses and {warClan.WarTies} draws");
+                // Access global and local rankings
+                var locationsExamples = new LocationsExamples(token);
+                await locationsExamples.ListLocations();
+                await locationsExamples.GetClanRankingsForASpecificLocation();
             }
             catch (ClashOfClansException ex)
             {
@@ -65,8 +45,8 @@ namespace ClashOfClans.App
         private static void AddConfigurationValues(out string clanTag, out string playerTag, out string token)
         {
             var config = new ConfigurationBuilder()
-                    .AddJsonFile("appsettings.json")
-                    .Build();
+                .AddJsonFile("appsettings.json")
+                .Build();
 
             token = config["api:token"];
             clanTag = config["clanTag"];
