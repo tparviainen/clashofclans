a74a4aa129db8a7d5ed8c35cb2fbf4b8105b4446
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-10-16 20:39:23 +0300

    Add Label/LabelList models

---
+++ src/ClashOfClans/Models/Label.cs
@@ -0,0 +1,11 @@
+﻿namespace ClashOfClans.Models
+{
+    public class Label
+    {
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public UrlContainer IconUrls { get; set; }
+    }
+}
---
+++ src/ClashOfClans/Models/LabelList.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace ClashOfClans.Models
+{
+    public class LabelList : List<Label>
+    {
+    }
+}
