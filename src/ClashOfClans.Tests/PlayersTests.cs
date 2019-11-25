3fa1fd5ba5ebac2194cbbe647127c583f2a28ffa
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-11-25 17:06:56 +0200

    Add test for SC API throttling

--- src/ClashOfClans.Tests/PlayersTests.cs
+++ src/ClashOfClans.Tests/PlayersTests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ClashOfClans.Tests
@@ -18,6 +21,36 @@ namespace ClashOfClans.Tests
 
                 // Assert
                 Assert.IsNotNull(player);
+            }
+        }
+
+        [TestMethod]
+        public async Task GetPlayerInformationInALoopWithoutThrottling()
+        {
+            // Arrange
+            var playerTag = PlayerTags.First();
+            var requestCount = 200;
+            var requestsPerSecond = 50;
+            var sw = new Stopwatch();
+            _coc.Configure(options =>
+            {
+                Assert.IsTrue(options.MaxRequestsPerSecond == requestsPerSecond);
+            });
+
+            // Act
+            try
+            {
+                sw.Start();
+                var results = Enumerable.Range(1, requestCount).Select(i => _coc.Players.GetPlayerAsync(playerTag));
+                _ = await Task.WhenAll(results);
+                sw.Stop();
+
+                Trace.WriteLine($"{requestCount} requests took {sw.ElapsedMilliseconds} ms with {requestsPerSecond} requests/second rate");
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.Fail(ex.ToString());
             }
         }
     }
