99002e5207224f2f2513353cba192c548051e1c8
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-11-25 18:57:01 +0200

    Fix #14 HttpClient is intended to be instantiated once per application

--- src/ClashOfClans/Core/Net/ApiEndpoint.cs
+++ src/ClashOfClans/Core/Net/ApiEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -10,25 +11,25 @@ namespace ClashOfClans.Core.Net
     /// </summary>
     internal class ApiEndpoint
     {
-        private readonly AuthenticationHeaderValue _authorization;
-        private readonly Uri _baseAddress = new Uri("https://api.clashofclans.com/v1/");
+        // HttpClient is intended to be instantiated once and re-used throughout the life of an application
+        private readonly HttpClient _client = new HttpClient();
 
         public ApiEndpoint(string token)
         {
-            _authorization = new AuthenticationHeaderValue("Bearer", token);
+            _client.BaseAddress = new Uri("https://api.clashofclans.com/v1/");
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // Sets the number of milliseconds after which an active ServicePoint connection is closed
+            var sp = ServicePointManager.FindServicePoint(_client.BaseAddress);
+            sp.ConnectionLeaseTimeout = (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
         }
 
         public async Task<HttpResponseMessage> GetMessageAsync(string requestUri)
         {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = _baseAddress;
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = _authorization;
-
-                return await client.GetAsync(requestUri);
-            }
+            return await _client.GetAsync(requestUri);
         }
     }
 }
