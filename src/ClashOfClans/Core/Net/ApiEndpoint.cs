7053903b29074e8f961fa8849a76b1b92fb29af2
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-11-25 17:06:02 +0200

    Create only one HttpClient

--- src/ClashOfClans/Core/Net/ApiEndpoint.cs
+++ src/ClashOfClans/Core/Net/ApiEndpoint.cs
@@ -10,25 +10,21 @@ namespace ClashOfClans.Core.Net
     /// </summary>
     internal class ApiEndpoint
     {
-        private readonly AuthenticationHeaderValue _authorization;
-        private readonly Uri _baseAddress = new Uri("https://api.clashofclans.com/v1/");
+        // HttpClient is intended to be instantiated once and re-used throughout the life of an application
+        private static readonly HttpClient _client = new HttpClient();
 
         public ApiEndpoint(string token)
         {
-            _authorization = new AuthenticationHeaderValue("Bearer", token);
+            _client.BaseAddress = new Uri("https://api.clashofclans.com/v1/");
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
 
         public async Task<HttpResponseMessage> GetMessageAsync(string requestUri)
         {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = _baseAddress;
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = _authorization;
-
-                return await client.GetAsync(requestUri);
-            }
+            return await _client.GetAsync(requestUri);
         }
     }
 }
