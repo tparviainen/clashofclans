75d974bd009dbd7d9a542324eec7d9ab951bbf9d
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-11-28 17:51:04 +0200

    Move constants to own file

---
+++ src/ClashOfClans/Core/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace ClashOfClans.Core
+{
+    internal static class Constants
+    {
+        /// <summary>
+        /// CWL group that is in 'Preparation' state contains invalid war tags
+        /// </summary>
+        public const string InvalidWarTag = "#0";
+
+        /// <summary>
+        /// All tags (war tag, clan tag, player tag) starts with '#' character
+        /// </summary>
+        public const string TagStartChar = "#";
+
+        /// <summary>
+        /// If name is used as part of search query, it needs to be at least
+        /// three characters long.
+        /// </summary>
+        public const int MinQueryNameLength = 3;
+    }
+}
