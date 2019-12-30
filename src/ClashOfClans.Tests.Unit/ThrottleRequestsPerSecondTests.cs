346906e1fc8d43ab0bdaf07d42569ccd3f5808ae
Tomi Parviainen <tomi.parviainen@gmail.com>
2019-12-30 16:53:36 +0200

    Add .NET Core 2.2 Unit Test Project

---
+++ src/ClashOfClans.Tests.Unit/ThrottleRequestsPerSecondTests.cs
@@ -0,0 +1,66 @@
+using ClashOfClans.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ClashOfClans.Tests.Unit
+{
+    [TestClass]
+    public class ThrottleRequestsPerSecondTests
+    {
+        [TestMethod]
+        public async Task WaitedCallsAreThrottled()
+        {
+            // Arrange
+            var maxRequestsPerSecond = 20;
+            var throttleRequests = new ThrottleRequestsPerSecond(maxRequestsPerSecond);
+            var totalTime = new TimeSpan(0, 0, 1);
+            var loopCount = 0;
+
+            // Act
+            var stopWatch = Stopwatch.StartNew();
+
+            while (true)
+            {
+                await throttleRequests.WaitAsync();
+
+                if (stopWatch.Elapsed >= totalTime)
+                {
+                    stopWatch.Stop();
+                    break;
+                }
+
+                loopCount++;
+            }
+
+            // Assert
+            Assert.IsTrue(loopCount <= maxRequestsPerSecond, $"Loopcount: {loopCount}");
+        }
+
+        [TestMethod]
+        public async Task NonWaitedCallsAreThrottled()
+        {
+            // Arrange
+            var rounds = 3;
+            var maxRequestsPerSecond = 50;
+            var throttleRequests = new ThrottleRequestsPerSecond(maxRequestsPerSecond);
+            var tasks = new List<Task>();
+
+            // Act
+            var stopWatch = Stopwatch.StartNew();
+            for (int i = 0; i < rounds * maxRequestsPerSecond; i++)
+            {
+                tasks.Add(throttleRequests.WaitAsync());
+            }
+
+            await Task.WhenAll(tasks);
+            var elapsed = stopWatch.ElapsedMilliseconds;
+
+            // Assert
+            var delayForTwoRequests = 1000 / maxRequestsPerSecond * 2;
+            Assert.IsTrue(elapsed > (rounds * 1000 - delayForTwoRequests), $"Elapsed: {elapsed}");
+        }
+    }
+}
--- src/ClashOfClans/Properties/AssemblyInfo.cs
+++ src/ClashOfClans/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ClashOfClans.Tests")]
+[assembly: InternalsVisibleTo("ClashOfClans.Tests.Unit")]
