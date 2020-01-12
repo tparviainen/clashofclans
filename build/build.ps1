aee4211f23b07abaa7b2af6da32e0d4208c402af
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-01-12 16:34:27 +0200

    Add workflow to push NuGet automatically on version change

--- build/build.ps1
+++ build/build.ps1
@@ -1,7 +1,9 @@
 # https://docs.microsoft.com/en-us/nuget/concepts/package-versioning : Major.Minor.Patch[-Suffix]
-$packageVersion = "6.0.4"
+$packageVersion = Get-Content .\version.txt -Tail 1
 $project = "..\src\ClashOfClans\ClashOfClans.csproj"
 $configuration = "Release"
 
 dotnet clean -c $configuration $project
 dotnet pack -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
+
+nuget push ..\src\ClashOfClans\bin\$configuration\ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
