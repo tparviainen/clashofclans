bf7cd5968eabc7972906fc16f9cd98f1b17fd8b4
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-04-19 15:56:32 +0300

    Create NuGet package from nuget cli

--- build/build.ps1
+++ build/build.ps1
@@ -4,6 +4,7 @@ $project = "..\src\ClashOfClans\ClashOfClans.csproj"
 $configuration = "Release"
 
 dotnet clean -c $configuration $project
-dotnet pack -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
+dotnet build -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
 
-nuget push ..\src\ClashOfClans\bin\$configuration\ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
+nuget pack $project -Version $packageVersion -Properties Configuration=$configuration -IncludeReferencedProjects
+nuget push ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
