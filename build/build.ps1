85da7e6b7bffeae591d158b734093b8bb62a957a
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-04-20 21:19:53 +0300

    Use dotnet.exe to pack a project

--- build/build.ps1
+++ build/build.ps1
@@ -4,7 +4,6 @@ $project = "..\src\ClashOfClans\ClashOfClans.csproj"
 $configuration = "Release"
 
 dotnet clean -c $configuration $project
-dotnet build -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
+dotnet pack -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
 
-nuget pack $project -Version $packageVersion -Properties Configuration=$configuration -IncludeReferencedProjects
-nuget push ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
+nuget push ..\src\ClashOfClans\bin\$configuration\ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
