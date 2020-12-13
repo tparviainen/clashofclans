8cf6638cf8b70dc6612f1659aaac863a37953ff7
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-12-13 12:00:05 +0200

    Publish symbol package to nuget.org

--- build/build.ps1
+++ build/build.ps1
@@ -1,9 +1,14 @@
 # https://docs.microsoft.com/en-us/nuget/concepts/package-versioning : Major.Minor.Patch[-Suffix]
 $packageVersion = Get-Content .\version.txt -Tail 1
-$project = "..\src\ClashOfClans\ClashOfClans.csproj"
+$packageId = "ClashOfClans"
+$basePath = "..\src\ClashOfClans\"
+$project =  $basePath + $packageId + ".csproj"
+$nuspec = $basePath + $packageId + ".nuspec"
+$nupkg = "$packageId.$packageVersion.nupkg"
 $configuration = "Release"
 
 dotnet clean -c $configuration $project
-dotnet pack -p:PackageVersion=$packageVersion -p:Version=$packageVersion -c $configuration $project
+dotnet build -c $configuration $project -p:Version=$packageVersion
 
-nuget push ..\src\ClashOfClans\bin\$configuration\ClashOfClans.$packageVersion.nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
+nuget pack $nuspec -Properties Version=$packageVersion -Symbols -SymbolPackageFormat snupkg
+nuget push $nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
