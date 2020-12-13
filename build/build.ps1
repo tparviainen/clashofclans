671ae848f376030b49876ca0d7e1093063286160
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-12-13 14:14:22 +0200

    Add support for Source Link

--- build/build.ps1
+++ build/build.ps1
@@ -1,14 +1,16 @@
-# https://docs.microsoft.com/en-us/nuget/concepts/package-versioning : Major.Minor.Patch[-Suffix]
-$packageVersion = Get-Content .\version.txt -Tail 1
+. .\functions.ps1
+
+$version = Get-PackageVersion
 $packageId = "ClashOfClans"
 $basePath = "..\src\ClashOfClans\"
 $project =  $basePath + $packageId + ".csproj"
 $nuspec = $basePath + $packageId + ".nuspec"
-$nupkg = "$packageId.$packageVersion.nupkg"
+$nupkg = "$packageId.$version.nupkg"
 $configuration = "Release"
+$commit = Get-CommitHash
 
 dotnet clean -c $configuration $project
-dotnet build -c $configuration $project -p:Version=$packageVersion
+dotnet build -c $configuration $project -p:Version=$version -p:ContinuousIntegrationBuild=true
 
-nuget pack $nuspec -Properties Version=$packageVersion -Symbols -SymbolPackageFormat snupkg
+nuget pack $nuspec -Properties "Version=$version;Commit=$commit" -Symbols -SymbolPackageFormat snupkg
 nuget push $nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
