45501cddc50bca51ae48df09c5822aa4b36e58e0
Tomi Parviainen <tomi.parviainen@gmail.com>
2020-12-21 16:27:48 +0200

    Draft implementation of AddClashOfClans method

---
+++ src/ClashOfClans/Extensions/ClashOfClansExtensions.cs
@@ -0,0 +1,19 @@
+﻿using ClashOfClans;
+using ClashOfClans.Core;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ClashOfClansExtensions
+    {
+        public static IServiceCollection AddClashOfClans(this IServiceCollection services, Action<ClashOfClansOptions> configureOptions)
+        {
+            services.AddOptions();
+            services.Configure(configureOptions);
+
+            services.AddSingleton<IClashOfClans, ClashOfClansClient>();
+
+            return services;
+        }
+    }
+}
