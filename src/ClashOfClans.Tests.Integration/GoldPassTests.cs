ef05f0cc23b1435639ce8c0c3cb34986af28f4a7
Tomi Parviainen <tomi.parviainen@gmail.com>
2021-04-02 15:26:31 +0300

    Add try/catch because the SC API is broken ATM

--- src/ClashOfClans.Tests.Integration/GoldPassTests.cs
+++ src/ClashOfClans.Tests.Integration/GoldPassTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ClashOfClans.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ClashOfClans.Tests.Integration
@@ -9,13 +12,26 @@ namespace ClashOfClans.Tests.Integration
         [TestMethod]
         public async Task GetCurrentGoldPassSeason()
         {
-            // Arrange
+            try
+            {
+                // Arrange
 
-            // Act
-            var goldPassSeason = await _coc.GoldPass.GetCurrentGoldPassSeasonAsync();
+                // Act
+                var goldPassSeason = await _coc.GoldPass.GetCurrentGoldPassSeasonAsync();
 
-            // Assert
-            Assert.IsNotNull(goldPassSeason);
+                // Assert
+                Assert.IsNotNull(goldPassSeason);
+            }
+            catch (ClashOfClansException ex)
+            {
+                Trace.WriteLine($"ClashOfClansException reason: {ex.Error.Reason}, message: {ex.Error.Message}");
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                Assert.Fail();
+            }
         }
     }
 }
