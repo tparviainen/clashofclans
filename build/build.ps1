4c126fb41e41a222ba0bad2bd56e966e64ca3558
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-07-26 14:07:58 +0300

    Set branch name based on current branch

--- build/build.ps1
+++ build/build.ps1
@@ -8,9 +8,10 @@ $nuspec = $basePath + $packageId + ".nuspec"
 $nupkg = "$packageId.$version.nupkg"
 $configuration = "Release"
 $commit = Get-CommitHash
+$branch = Get-BranchName
 
 dotnet clean -c $configuration $project
 dotnet build -c $configuration $project -p:Version=$version -p:ContinuousIntegrationBuild=true
 
-nuget pack $nuspec -Properties "Version=$version;Commit=$commit" -Symbols -SymbolPackageFormat snupkg
+nuget pack $nuspec -Properties "Version=$version;Commit=$commit;Branch=$branch" -Symbols -SymbolPackageFormat snupkg
 nuget push $nupkg -ApiKey ${env:NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json
