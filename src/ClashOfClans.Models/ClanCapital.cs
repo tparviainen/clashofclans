595f8587c149d253a142a4ebba76d7c236936099
Tomi Parviainen <tomi.parviainen@gmail.com>
2022-10-01 14:05:39 +0300

    Add support for clan capital
    - Clan capital data for clan
    - Clan capital data for player

---
+++ src/ClashOfClans.Models/ClanCapital.cs
@@ -0,0 +1,11 @@
+﻿namespace ClashOfClans.Models
+{
+    public class ClanCapital
+    {
+        private int? _capitalHallLevel;
+
+        public int CapitalHallLevel { get => _capitalHallLevel ?? default; set => _capitalHallLevel = value; }
+
+        public ClanDistrictDataList Districts { get; set; } = default!;
+    }
+}
---
+++ src/ClashOfClans.Models/ClanDistrictData.cs
@@ -0,0 +1,14 @@
+﻿namespace ClashOfClans.Models
+{
+    public class ClanDistrictData
+    {
+        private int? _id;
+        private int? _districtHallLevel;
+
+        public int Id { get => _id ?? default; set => _id = value; }
+
+        public string Name { get; set; } = default!;
+
+        public int DistrictHallLevel { get => _districtHallLevel ?? default; set => _districtHallLevel = value; }
+    }
+}
